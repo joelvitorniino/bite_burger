5b5fa7834b051dc21891e8fc659c669acff628a3
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-06-12 18:59:03 -0300

    feature: menubar completed

--- src/app/component/MenuBar.tsx
+++ src/app/component/MenuBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function MenuBar() {
     return (
-        <nav className="w-[1440px] h-[136px] lg:w-[1600px] bg-white-gray relative">
+        <nav className="w-full h-[136px] bg-white-gray relative">
             <ul className="flex items-center justify-start h-full list-none p-0 m-0">
                 <div className="rounded-full bg-gray-200 w-29 flex items-center justify-center">
                     <Image src="/images/logo.png" alt="Bite Burger Logo" width="135" height="135" className="rounded-full" />
@@ -17,11 +17,11 @@ export default function MenuBar() {
                 <li className="font-inter font-extrabold italic text-3xl text-black sm:ml-11 lg:ml-11">
                     <Link href="/" className="list-none">Sobre</Link>
                 </li>
-                <li className="font-inter font-medium text-3xl text-red-light text-opacity-[1] lg:ml-[720px]">
+                <li className="font-inter font-medium text-3xl text-red-light text-opacity-[1] ml-auto">
                     <Link href="/">criar conta</Link>
                 </li>
-                <li className="flex items-center justify-center">
-                    <Link href="/" className="font-inter font-medium text-3xl text-white w-[200px] h-[60px] bg-red-light border-[6px] border-red-dark rounded-[6px] flex items-center justify-center">Entrar</Link>
+                <li className="flex items-center justify-center ml-4">
+                    <Link href="/" className="font-inter font-medium text-3xl text-white bg-red-light border-4 border-red-dark rounded-md px-6 py-3 flex items-center justify-center hover:bg-red-dark transition duration-300">Entrar</Link>
                 </li>
             </ul>
         </nav>
